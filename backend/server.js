d8409b93c7a60b9bbcccae14720c76f9f653e94d
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2023-01-18 10:40:31 +0530

    first changes

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express  = require('express');
+const dotenv  = require('dotenv');
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000, console.log("server started"));
