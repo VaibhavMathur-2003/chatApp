6d4364f18964548c4d61edf23e691e2fa8bad49c
VaibhavMathur03 <mathurvaibhav010@gmail.com>
2023-01-27 09:57:54 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const express  = require('express');
 const dotenv  = require('dotenv');
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 dotenv.config();
+connectDB();
 const app = express();
+
+app.use(express.json());
+app.use(notFound)
+app.use(errorHandler)
+
+app.use('/api/user', userRoutes )
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log("server started"));
